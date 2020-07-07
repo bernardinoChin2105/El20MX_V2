cbfd8c023da66f10023acf19d8bd4353a494331f
Yelmy Pech <yelmymc@hotmail.com>
2020-07-07 17:06:44 -0500

    Req7 Nuevos datos para integración

---
+++ MVC_Project.Integrations/Paybook/PayBook.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_Project.Integrations.Paybook
+{
+    public class Paybook
+    {
+        //public static string CallServicePaybook(string urlService, Object JsonString, string method)
+        //{
+        //    string urlapi = ConfigurationManager.AppSettings["SATws.Url"];
+        //    string apiKey = ConfigurationManager.AppSettings["SATws.ApiKey"];
+
+
+        //    return null;
+        //}
+    }
+}
