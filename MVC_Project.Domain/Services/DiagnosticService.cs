c26dd29b91579cb3e60be0613c27879da66e64e1
Manuel Anguas <manuel@ensitech.com>
2020-07-15 18:37:29 -0500

    Req6 Que se cambie a mayusculas en automatico el rfc, Reducir cantidad de caracteres en contraseña https://prnt.sc/thtqok Poner longitud máxima de 20, Cuando le das buscar sin ingresar una fecha, se queda la palabra processing https://prnt.sc/thv5gh  Validar que filtre bien las fechas

--- MVC_Project.Domain/Services/DiagnosticService.cs
+++ MVC_Project.Domain/Services/DiagnosticService.cs
@@ -4,6 +4,7 @@ using MVC_Project.Domain.Repositories;
 using NHibernate.Transform;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,14 +26,24 @@ namespace MVC_Project.Domain.Services
 
         public List<DiagnosticsList> DiagnosticList(string uuid, BasePagination pagination)
         {
+            String dateinit = null;
+            String dateend = null;
+            if (pagination.CreatedOnStart != null)
+            {
+                dateinit = Convert.ToDateTime(pagination.CreatedOnStart).ToString("yyyy-MM-dd", DateTimeFormatInfo.InvariantInfo);
+            }
+            if (pagination.CreatedOnEnd != null)
+            {
+                dateend = Convert.ToDateTime(pagination.CreatedOnEnd).ToString("yyyy-MM-dd", DateTimeFormatInfo.InvariantInfo);
+            }
             var list = _repository.Session.CreateSQLQuery("exec dbo.st_diagnosticList " +
                 "@PageNum =:PageNum, @PageSize =:PageSize, @uuid =:uuid, @createdOnStart=:createdOnStart, @createdOnEnd=:createdOnEnd")
                     //.AddEntity(typeof(DiagnosticList))
                     .SetParameter("PageNum", pagination.PageNum)
                     .SetParameter("PageSize", pagination.PageSize)
                     .SetParameter("uuid", uuid)
-                    .SetParameter("createdOnStart", pagination.CreatedOnStart)
-                    .SetParameter("createdOnEnd", pagination.CreatedOnEnd)
+                    .SetParameter("createdOnStart", dateinit)
+                    .SetParameter("createdOnEnd", dateend)
                     .SetResultTransformer(NHibernate.Transform.Transformers.AliasToBean(typeof(DiagnosticsList)))
                     .List<DiagnosticsList>();
                     //.SetResultSetMapping()
