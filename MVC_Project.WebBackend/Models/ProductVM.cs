fe0d350fbcd9efaeb94e7954a93930c385ad4e71
Yelmy Pech <yelmymc@hotmail.com>
2020-09-07 10:37:55 -0500

    Req9 Listado de Planes

---
+++ MVC_Project.WebBackend/Models/ProductVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Project.WebBackend.Models
+{
+    public class ProductVM
+    {
+        
+    }
+
+    public class PlansFilterViewModel
+    {        
+        [Display(Name = "Nombre")]
+        public string Name { get; set; }
+    }
+}
