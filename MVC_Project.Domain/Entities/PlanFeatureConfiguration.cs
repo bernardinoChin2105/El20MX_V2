27093df3911ae40bf1088b6abf2feb63755da9e2
wcaamal <wcaamal@gmail.com>
2020-09-11 12:22:20 -0500

    Req9 se crean las tablas de caracteristicas del plan

---
+++ MVC_Project.Domain/Entities/PlanFeatureConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_Project.Domain.Entities
+{
+    public class PlanFeatureConfiguration
+    {
+        public virtual Int64 id { get; set; }
+        public virtual Guid uuid { get; set; }
+
+        public virtual Plan plan { get; set; }
+        public virtual PlanFeature planFeature { get; set; }
+
+        public virtual string value1 { get; set; }
+        public virtual string value2 { get; set; } //valor2 para rangos
+
+        public virtual DateTime createdAt { get; set; }
+        public virtual DateTime modifiedAt { get; set; }
+        public virtual string status { get; set; }
+    }
+}
