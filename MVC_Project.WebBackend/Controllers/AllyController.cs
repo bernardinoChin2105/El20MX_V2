b62eb05332051971e643f7eb864a9c2f8c995cbf
Yelmy Pech <yelmymc@hotmail.com>
2020-09-18 08:26:56 -0500

    Req11 Se integraron nuevas pantallas

--- MVC_Project.WebBackend/Controllers/AllyController.cs
+++ MVC_Project.WebBackend/Controllers/AllyController.cs
@@ -1,6 +1,8 @@
 ﻿using LogHubSDK.Models;
+using MVC_Project.Domain.Entities;
 using MVC_Project.Domain.Model;
 using MVC_Project.Domain.Services;
+using MVC_Project.FlashMessages;
 using MVC_Project.Utils;
 using MVC_Project.WebBackend.AuthManagement;
 using MVC_Project.WebBackend.Models;
@@ -99,7 +101,90 @@ namespace MVC_Project.WebBackend.Controllers
         [AllowAnonymous]
         public ActionResult Create()
         {
-            return View();         
+            return View();
+        }
+
+        [Authorize, HttpPost, ValidateAntiForgeryToken, ValidateInput(true)]
+        public ActionResult Create(AllyFilterViewModel model)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                    throw new Exception("El modelo de entrada no es válido");
+
+                var authUser = Authenticator.AuthenticatedUser;
+
+                if (_allyService.FindBy(x => x.name == model.Name).Any())
+                    throw new Exception("Ya existe un Aliado con el Nombre proporcionado");
+
+                DateTime todayDate = DateUtil.GetDateTimeNow();
+
+                Ally ally = new Ally()
+                {
+                    uuid = Guid.NewGuid(),
+                    name = model.Name,
+                    createdAt = todayDate,
+                    modifiedAt = todayDate,
+                    status = SystemStatus.ACTIVE.ToString(),
+                };
+                
+                _allyService.Create(ally);
+                MensajeFlashHandler.RegistrarMensaje("Registro exitoso", TiposMensaje.Success);
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {                
+                return View(model);
+            }
+        }
+
+        public ActionResult Edit(string uuid)
+        {
+            try
+            {
+                AllyFilterViewModel model = new AllyFilterViewModel();
+                var userAuth = Authenticator.AuthenticatedUser;
+
+                var ally = _allyService.FirstOrDefault(x => x.uuid == Guid.Parse(uuid));
+                if (ally == null)
+                    throw new Exception("El registro de Aliado no se encontró en la base de datos");
+
+                model.Id = ally.id;
+                model.Name = ally.name;             
+                return View(model);
+            }
+            catch (Exception ex)
+            {
+                MensajeFlashHandler.RegistrarMensaje(ex.Message.ToString(), TiposMensaje.Error);
+                return RedirectToAction("Index");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Edit(AllyFilterViewModel model)
+        {
+            var userAuth = Authenticator.AuthenticatedUser;            
+            var allyData = _allyService.FirstOrDefault(x => x.id == model.Id);
+            try
+            {
+                if (!ModelState.IsValid)
+                    throw new Exception("El modelo de entrada no es válido");
+                
+                DateTime todayDate = DateUtil.GetDateTimeNow();
+
+                allyData.name = model.Name;
+                allyData.modifiedAt = todayDate;
+                allyData.status = SystemStatus.ACTIVE.ToString();
+        
+                _allyService.Update(allyData);
+                MensajeFlashHandler.RegistrarMensaje("Actualización exitosa", TiposMensaje.Success);
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                MensajeFlashHandler.RegistrarMensaje(ex.Message.ToString(), TiposMensaje.Error);
+                return View(model);
+            }
         }
     }
 }
