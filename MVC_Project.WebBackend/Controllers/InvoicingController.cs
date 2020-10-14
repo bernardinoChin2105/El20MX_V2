4f7b7fcbb97a1af96e1cd701cef0c0e94cdddb33
Yelmy Pech <yelmymc@hotmail.com>
2020-10-14 12:04:10 -0500

    Req15 Se comenzaron agregar algunos catalogos

--- MVC_Project.WebBackend/Controllers/InvoicingController.cs
+++ MVC_Project.WebBackend/Controllers/InvoicingController.cs
@@ -1,4 +1,6 @@
-﻿using MVC_Project.WebBackend.Models;
+﻿using MVC_Project.FlashMessages;
+using MVC_Project.Utils;
+using MVC_Project.WebBackend.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,6 +20,32 @@ namespace MVC_Project.WebBackend.Controllers
         public ActionResult Invoice()
         {
             InvoiceViewModel model = new InvoiceViewModel();
+            try
+            {
+                //stateList.Insert(0, (new SelectListItem() { Text = "Seleccione...", Value = "-1" }));
+
+                var regimenList = Enum.GetValues(typeof(TipoComprobante)).Cast<TipoComprobante>()
+                    .Select(e => new SelectListItem
+                    {
+                        Value = e.ToString(),
+                        Text = EnumUtils.GetDisplayName(e)
+                    });
+
+                //model.ListRegimen = new SelectList(regimenList);
+                //model.ListState = new SelectList(stateList);
+
+                /*
+                ((TipoComprobante)Enum.Parse(typeof(TipoComprobante), varTipoComprobante, true)).GetDescription();
+                ((MetodoPago)Enum.Parse(typeof(MetodoPago), varMetodoPago, true)).GetDescription(),
+                ((UsoCFDI)Enum.Parse(typeof(UsoCFDI), varUsoCFDI, true)).GetDescription();
+                 
+                 */
+            }
+            catch(Exception ex)
+            {
+                //string error = ex.Message.ToString();
+                MensajeFlashHandler.RegistrarMensaje(ex.Message.ToString(), TiposMensaje.Error);
+            }
             return View(model);
         }
     }
