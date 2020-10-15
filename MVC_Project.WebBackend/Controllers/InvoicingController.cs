c92192719c7f342559283f137cdbd2bc419b9ee1
Yelmy Pech <yelmymc@hotmail.com>
2020-10-15 08:41:24 -0500

    Req15 Se agregaron enum

--- MVC_Project.WebBackend/Controllers/InvoicingController.cs
+++ MVC_Project.WebBackend/Controllers/InvoicingController.cs
@@ -24,24 +24,56 @@ namespace MVC_Project.WebBackend.Controllers
             {
                 //stateList.Insert(0, (new SelectListItem() { Text = "Seleccione...", Value = "-1" }));
 
-                var regimenList = Enum.GetValues(typeof(TipoComprobante)).Cast<TipoComprobante>()
+                //Tipo de Comprobante
+                var TypeVoucher = Enum.GetValues(typeof(TipoComprobante)).Cast<TipoComprobante>()
                     .Select(e => new SelectListItem
                     {
                         Value = e.ToString(),
-                        Text = EnumUtils.GetDisplayName(e)
-                    });
+                        Text = EnumUtils.GetDescription(e)
+                    }).Where(x => x.Value != "N" & x.Value != "T");
 
-                //model.ListRegimen = new SelectList(regimenList);
-                //model.ListState = new SelectList(stateList);
+                //Forma de pago
+                var PaymentForm = Enum.GetValues(typeof(MetodoPago)).Cast<MetodoPago>()
+                   .Select(e => new SelectListItem
+                   {
+                       Value = e.ToString(),
+                       Text = EnumUtils.GetDescription(e)
+                   });
 
-                /*
-                ((TipoComprobante)Enum.Parse(typeof(TipoComprobante), varTipoComprobante, true)).GetDescription();
-                ((MetodoPago)Enum.Parse(typeof(MetodoPago), varMetodoPago, true)).GetDescription(),
-                ((UsoCFDI)Enum.Parse(typeof(UsoCFDI), varUsoCFDI, true)).GetDescription();
-                 
-                 */
+                //Metodo de pago
+                var PaymentMethod = Enum.GetValues(typeof(MetodoPago)).Cast<MetodoPago>()
+                  .Select(e => new SelectListItem
+                  {
+                      Value = e.ToString(),
+                      Text = EnumUtils.GetDescription(e)
+                  });
+
+                //Uso de CFDI
+                var UseCFDI = Enum.GetValues(typeof(UsoCFDI)).Cast<UsoCFDI>()
+                  .Select(e => new SelectListItem
+                  {
+                      Value = e.ToString(),
+                      Text = EnumUtils.GetDescription(e)
+                  });
+
+                //Moneda
+                var Currency = Enum.GetValues(typeof(TypeCurrency)).Cast<TypeCurrency>()
+                  .Select(e => new SelectListItem
+                  {
+                      Value = e.ToString(),
+                      Text = EnumUtils.GetDescription(e)
+                  });
+
+                
+
+                model.ListTypeVoucher = new SelectList(TypeVoucher);
+                model.ListPaymentMethod = new SelectList(PaymentForm);
+                model.ListUseCFDI = new SelectList(PaymentForm);
+                model.ListPaymentMethod = new SelectList(PaymentMethod);
+                model.ListCurrency = new SelectList(Currency);
+
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 //string error = ex.Message.ToString();
                 MensajeFlashHandler.RegistrarMensaje(ex.Message.ToString(), TiposMensaje.Error);
