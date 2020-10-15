30b82ca3f45f06c6acd5f4d627019c4eceedd603
Yelmy Pech <yelmymc@hotmail.com>
2020-10-15 11:44:10 -0500

    Req15 Se comenzaron las creaciones de las tablas para los catalogos

---
+++ MVC_Project.Domain/Entities/ProductServiceKey.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_Project.Domain.Entities
+{
+    public class ProductServiceKey : IEntity
+    {
+        public virtual Int64 id { get; set; }
+        public virtual string code { get; set; }
+        public virtual string description { get; set; }
+
+        public virtual string includeIVATransferred { get; set; }
+        public virtual string includeIEPSTranslated { get; set; }       
+        public virtual int borderStripStimulus { get; set; }
+        public virtual string similarWords { get; set; }
+    }
+}
--- MVC_Project.WebBackend/Controllers/InvoicingController.cs
+++ MVC_Project.WebBackend/Controllers/InvoicingController.cs
@@ -20,64 +20,64 @@ namespace MVC_Project.WebBackend.Controllers
         public ActionResult Invoice()
         {
             InvoiceViewModel model = new InvoiceViewModel();
-            try
-            {
-                //stateList.Insert(0, (new SelectListItem() { Text = "Seleccione...", Value = "-1" }));
+            //try
+            //{
+            //    //stateList.Insert(0, (new SelectListItem() { Text = "Seleccione...", Value = "-1" }));
 
-                //Tipo de Comprobante
-                var TypeVoucher = Enum.GetValues(typeof(TipoComprobante)).Cast<TipoComprobante>()
-                    .Select(e => new SelectListItem
-                    {
-                        Value = e.ToString(),
-                        Text = EnumUtils.GetDescription(e)
-                    }).Where(x => x.Value != "N" & x.Value != "T");
+            //    //Tipo de Comprobante
+            //    var TypeVoucher = Enum.GetValues(typeof(TipoComprobante)).Cast<TipoComprobante>()
+            //        .Select(e => new SelectListItem
+            //        {
+            //            Value = e.ToString(),
+            //            Text = EnumUtils.GetDescription(e)
+            //        }).Where(x => x.Value != "N" & x.Value != "T");
 
-                //Forma de pago
-                var PaymentForm = Enum.GetValues(typeof(MetodoPago)).Cast<MetodoPago>()
-                   .Select(e => new SelectListItem
-                   {
-                       Value = e.ToString(),
-                       Text = EnumUtils.GetDescription(e)
-                   });
+            //    //Forma de pago
+            //    var PaymentForm = Enum.GetValues(typeof(MetodoPago)).Cast<MetodoPago>()
+            //       .Select(e => new SelectListItem
+            //       {
+            //           Value = e.ToString(),
+            //           Text = EnumUtils.GetDescription(e)
+            //       });
 
-                //Metodo de pago
-                var PaymentMethod = Enum.GetValues(typeof(MetodoPago)).Cast<MetodoPago>()
-                  .Select(e => new SelectListItem
-                  {
-                      Value = e.ToString(),
-                      Text = EnumUtils.GetDescription(e)
-                  });
+            //    //Metodo de pago
+            //    var PaymentMethod = Enum.GetValues(typeof(MetodoPago)).Cast<MetodoPago>()
+            //      .Select(e => new SelectListItem
+            //      {
+            //          Value = e.ToString(),
+            //          Text = EnumUtils.GetDescription(e)
+            //      });
 
-                //Uso de CFDI
-                var UseCFDI = Enum.GetValues(typeof(UsoCFDI)).Cast<UsoCFDI>()
-                  .Select(e => new SelectListItem
-                  {
-                      Value = e.ToString(),
-                      Text = EnumUtils.GetDescription(e)
-                  });
+            //    //Uso de CFDI
+            //    var UseCFDI = Enum.GetValues(typeof(UsoCFDI)).Cast<UsoCFDI>()
+            //      .Select(e => new SelectListItem
+            //      {
+            //          Value = e.ToString(),
+            //          Text = EnumUtils.GetDescription(e)
+            //      });
 
-                //Moneda
-                var Currency = Enum.GetValues(typeof(TypeCurrency)).Cast<TypeCurrency>()
-                  .Select(e => new SelectListItem
-                  {
-                      Value = e.ToString(),
-                      Text = EnumUtils.GetDescription(e)
-                  });
+            //    //Moneda
+            //    var Currency = Enum.GetValues(typeof(TypeCurrency)).Cast<TypeCurrency>()
+            //      .Select(e => new SelectListItem
+            //      {
+            //          Value = e.ToString(),
+            //          Text = EnumUtils.GetDescription(e)
+            //      });
 
                 
 
-                model.ListTypeVoucher = new SelectList(TypeVoucher);
-                model.ListPaymentMethod = new SelectList(PaymentForm);
-                model.ListUseCFDI = new SelectList(PaymentForm);
-                model.ListPaymentMethod = new SelectList(PaymentMethod);
-                model.ListCurrency = new SelectList(Currency);
+            //    model.ListTypeVoucher = new SelectList(TypeVoucher);
+            //    model.ListPaymentMethod = new SelectList(PaymentForm);
+            //    model.ListUseCFDI = new SelectList(PaymentForm);
+            //    model.ListPaymentMethod = new SelectList(PaymentMethod);
+            //    model.ListCurrency = new SelectList(Currency);
 
-            }
-            catch (Exception ex)
-            {
-                //string error = ex.Message.ToString();
-                MensajeFlashHandler.RegistrarMensaje(ex.Message.ToString(), TiposMensaje.Error);
-            }
+            //}
+            //catch (Exception ex)
+            //{
+            //    //string error = ex.Message.ToString();
+            //    MensajeFlashHandler.RegistrarMensaje(ex.Message.ToString(), TiposMensaje.Error);
+            //}
             return View(model);
         }
     }
