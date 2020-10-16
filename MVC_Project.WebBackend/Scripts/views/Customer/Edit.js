ee6f1dc9b817151010a357f64d436d487753893d
wcaamal <wcaamal@gmail.com>
2020-10-16 09:05:01 -0500

    Req7 Se agrega la validacion de campo alfanumerico en la razon social cuando es PM

--- MVC_Project.WebBackend/Scripts/views/Customer/Edit.js
+++ MVC_Project.WebBackend/Scripts/views/Customer/Edit.js
@@ -69,6 +69,9 @@
             },
             InteriorNumber: {
                 Alphanumeric: true,
+            },
+            BusinessName: {
+                Alphanumeric: true
             }
         }
     });
