660764e8bf51e7cefebd8c4761f7ed9e080d835c
Yelmy Pech <yelmymc@hotmail.com>
2020-11-13 17:07:00 -0600

    Req8,9,11,15

--- MVC_Project.WebBackend/Scripts/views/Alliance/Index.js
+++ MVC_Project.WebBackend/Scripts/views/Alliance/Index.js
@@ -13,7 +13,26 @@ $("#btnClearForm").click(function () {
 });
 
 $(".btn-filter-rol").click(function () {
+    if (!$('#SearchForm').valid()) {
+        return;
+    }
     $('#table').DataTable().draw();
+});
+
+$.validator.addMethod("Alphanumeric",
+    function (value, element) {
+        return value.match(/^[A-Za-zÀ-ÿ\u00f1\u00d10-9 _.-]+$|^$/);
+    }, "El campo debe ser alfanumérico"
+);
+$("#SearchForm").validate({
+    rules: {
+        Name: {
+            Alphanumeric: true
+        },
+        AllyName: {
+            Alphanumeric: true
+        }
+    }
 });
 
 var AllianceIndexControlador = function (htmlTableId, baseUrl, editUrl, ActiveInactiveUrl, hasFullAccessController) {
@@ -78,7 +97,6 @@ var AllianceIndexControlador = function (htmlTableId, baseUrl, editUrl, ActiveIn
         }).on('xhr.dt', function (e, settings, data) {
             El20Utils.ocultarCargador();
             });
-
 
         $(this.htmlTable, "tbody").on('click',
             'td.menu-options .btn-group .btn-active',
@@ -173,5 +191,6 @@ var AllianceIndexControlador = function (htmlTableId, baseUrl, editUrl, ActiveIn
                         }
                     });
             });
+
     };
 };
