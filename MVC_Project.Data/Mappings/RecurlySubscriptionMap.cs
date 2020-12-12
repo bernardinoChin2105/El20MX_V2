930dd3760c2a4c291a45e03f80dec460d15b0dc7
Yelmy Pech <yelmymc@hotmail.com>
2020-12-11 23:19:14 -0600

    Req13 Creación de tablas y mapeo de información

---
+++ MVC_Project.Data/Mappings/RecurlySubscriptionMap.cs
@@ -0,0 +1,32 @@
+﻿using FluentNHibernate.Mapping;
+using MVC_Project.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_Project.Data.Mappings
+{
+    public class RecurlySubscriptionMap : ClassMap<RecurlySubscription>
+    {
+        public RecurlySubscriptionMap()
+        {
+            Table("recurlySubscription");
+            Id(x => x.id).GeneratedBy.Identity().Column("id");
+            Map(x => x.uuid).Column("uuid").Not.Nullable();
+
+            Map(x => x.subscriptionId).Column("subscriptionId").Not.Nullable();
+            Map(x => x.planId).Column("planId").Nullable();
+            Map(x => x.planCode).Column("planCode").Nullable();
+            Map(x => x.planName).Column("planName").Nullable();
+            Map(x => x.state).Column("state").Nullable();
+
+            Map(x => x.createdAt).Column("createdAt").Not.Nullable();
+            Map(x => x.modifiedAt).Column("modifiedAt").Not.Nullable();
+            Map(x => x.status).Column("status").Nullable();
+
+            References(x => x.account).Column("accountId").Nullable();
+        }
+    }
+}
---
+++ MVC_Project.Domain/Entities/RecurlyPayment.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_Project.Domain.Entities
+{
+    public class RecurlyPayment
+    {
+        public virtual Int64 id { get; set; }
+        public virtual decimal total { get; set; }
+        public virtual decimal subtotal { get; set; }
+        public virtual string paymentGateway { get; set; }
+        public virtual string statusCode { get; set; }
+        public virtual string statusMessage { get; set; }
+        public virtual string customerMessage { get; set; }
+        public virtual DateTime createdAt { get; set; }
+        /*Agregar datos por si me hacen falta*/
+
+        //public virtual DateTime modifiedAt { get; set; }
+        //public virtual string status { get; set; }
+
+        public virtual RecurlySubscription subscription { get; set; }
+    }
+}
