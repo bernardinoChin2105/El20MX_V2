254ae4fa8643fa279cf3c8c4728b6ff1c97b707d
wcaamal <wcaamal@gmail.com>
2020-12-17 15:30:23 -0600

    SinReq Se agrega el proceso de extraccion de facturas

---
+++ MVC_Project.Jobs/Jobs/SATExtractionJob.cs
@@ -0,0 +1,147 @@
+﻿using MVC_Project.Data.Helpers;
+using MVC_Project.Data.Repositories;
+using MVC_Project.Domain.Entities;
+using MVC_Project.Domain.Services;
+using MVC_Project.Integrations.Paybook;
+using MVC_Project.Integrations.SAT;
+using MVC_Project.Integrations.Storage;
+using MVC_Project.Utils;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Web;
+
+namespace MVC_Project.Jobs.Jobs
+{
+    public class SATExtractionJob
+    {
+        static bool executing = false;
+        static readonly Object thisLock = new Object();
+        static IProcessService _processService;
+        static IWebhookProcessService _webhookProcessService;
+        static IAccountService _accountService;
+        static ICustomerService _customerService;
+        static IProviderService _providerService;
+        static InvoiceIssuedService _invoicesIssuedService;
+        static InvoiceReceivedService _invoicesReceivedService;
+        static INotificationService _notificationService;
+        static ICredentialService _credentialService;
+
+        static readonly string JOB_CODE = "SATExtractionJob_GererateExtractions";
+
+        public static void GererateExtractions()
+        {
+            var _unitOfWork = new UnitOfWork();
+            _processService = new ProcessService(new Repository<Process>(_unitOfWork));
+            _webhookProcessService = new WebhookProcessService(new Repository<WebhookProcess>(_unitOfWork));
+            _accountService = new AccountService(new Repository<Account>(_unitOfWork));
+            _customerService = new CustomerService(new Repository<Customer>(_unitOfWork));
+            _providerService = new ProviderService(new Repository<Provider>(_unitOfWork));
+            _invoicesIssuedService = new InvoiceIssuedService(new Repository<InvoiceIssued>(_unitOfWork));
+            _invoicesReceivedService = new InvoiceReceivedService(new Repository<InvoiceReceived>(_unitOfWork));
+            _notificationService = new NotificationService(new Repository<Notification>(_unitOfWork));
+            _credentialService = new CredentialService(new Repository<Credential>(_unitOfWork));
+
+            Process processJob = _processService.GetByCode(JOB_CODE);
+            bool CAN_EXECUTE = processJob != null && processJob.Status && !processJob.Running; //Esta habilitado y no está corriendo (validacion por BD)
+
+            ProcessExecution processExecution = new ProcessExecution
+            {
+                Process = processJob,
+                StartAt = DateUtil.GetDateTimeNow(),
+                Status = true
+            };
+            _processService.CreateExecution(processExecution);
+
+            Boolean.TryParse(System.Configuration.ConfigurationManager.AppSettings["Jobs.EnabledJobs"], out bool NotificationProcessEnabled);
+            int.TryParse(System.Configuration.ConfigurationManager.AppSettings["Jobs.Attempt"], out int attempt);
+            StringBuilder strResult = new StringBuilder();
+
+            if (Monitor.TryEnter(thisLock))
+            {
+                try
+                {
+                    if (!executing && NotificationProcessEnabled && CAN_EXECUTE)
+                    {
+                        processJob.Running = true;
+                        _processService.Update(processJob);
+                        System.Diagnostics.Trace.TraceInformation(string.Format("[SATJob_SyncBills] Executing at {0}", DateUtil.GetDateTimeNow()));
+                        strResult.Append(string.Format("Executing at {0}", DateUtil.GetDateTimeNow()));
+
+                        #region Implementar logica de negocio especifica
+                        bool.TryParse(ConfigurationManager.AppSettings["SATws.IsHistoricalExtraction"], out bool isHistorical);
+                        var provider = ConfigurationManager.AppSettings["SATProvider"];
+
+                        var credentials = _credentialService.
+                            FindBy(x => x.credentialType == SATCredentialType.CIEC.ToString() &&
+                            x.provider == SystemProviders.SATWS.ToString() &&
+                            x.status == SystemStatus.ACTIVE.ToString() &&
+                            x.account.status == SystemStatus.ACTIVE.ToString()).Take(5);
+                        
+                        DateTime dateFrom = DateTime.UtcNow.AddMonths(-3);
+                        dateFrom = new DateTime(dateFrom.Year, dateFrom.Month, 1);
+
+                        if (isHistorical)
+                            dateFrom = new DateTime(2014, 1, 1);
+
+                        DateTime dateTo = DateTime.UtcNow;
+
+                        foreach (var credential in credentials)
+                        {
+                            try
+                            {
+                                string extractionId = SATService.GenerateExtractions(credential.account.rfc, dateFrom, dateTo, provider);
+                            }
+                            catch (Exception ex)
+                            {
+                            }
+                        }
+                        #endregion
+
+                        //UPDATE JOB DATABASE
+                        strResult.Append(string.Format("| End at {0}", DateUtil.GetDateTimeNow()));
+                        processExecution.EndAt = DateUtil.GetDateTimeNow();
+                        processExecution.Status = false;
+                        processExecution.Success = true;
+                        processExecution.Result = strResult.ToString();
+                        _processService.UpdateExecution(processExecution);
+
+                        processJob.Running = false;
+                        processJob.Result = strResult.ToString();
+                        processJob.LastExecutionAt = DateUtil.GetDateTimeNow();
+                        _processService.Update(processJob);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    System.Diagnostics.Debug.WriteLine(ex.Message);
+                    System.Diagnostics.Trace.TraceInformation(ex.Message);
+                    if (processJob != null)
+                    {
+                        strResult.Append(string.Format("ERROR: {0}", ex.Message));
+
+                        processExecution.EndAt = DateUtil.GetDateTimeNow();
+                        processExecution.Status = false;
+                        processExecution.Success = false;
+                        processExecution.Result = strResult.ToString();
+                        _processService.UpdateExecution(processExecution);
+
+                        processJob.Running = false;
+                        processJob.Result = strResult.ToString();
+                        processJob.LastExecutionAt = DateUtil.GetDateTimeNow();
+                        _processService.Update(processJob);
+                    }
+                }
+                finally
+                {
+                    executing = false;
+                    Monitor.Exit(thisLock);
+                }
+            }
+        }
+    }
+}
