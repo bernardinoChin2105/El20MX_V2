2b9747e8721b64c3a8e81eccefe1bec57af44f06
wcaamal <wcaamal@gmail.com>
2021-01-21 12:25:09 -0600

    SinReq Se agrega el guardado y validaciones al módulo de asignacion de CADs

--- MVC_Project.Domain/Services/CADAccountService.cs
+++ MVC_Project.Domain/Services/CADAccountService.cs
@@ -10,6 +10,7 @@ namespace MVC_Project.Domain.Services
 {
     public interface ICADAccountService : IService<CADAccount>
     {
+        void AssignCustomers(Int64 cadId, IEnumerable<Int64> inserts, IEnumerable<CADAccount> deletes);
     }
     public class CADAccountService : ServiceBase<CADAccount>, ICADAccountService
     {
@@ -17,6 +18,28 @@ namespace MVC_Project.Domain.Services
         public CADAccountService(IRepository<CADAccount> baseRepository) : base(baseRepository)
         {
             _repository = baseRepository;
+        }
+
+        public void AssignCustomers(Int64 cadId, IEnumerable<Int64> inserts, IEnumerable<CADAccount> deletes)
+        {
+            using (var transaction = _repository.Session.BeginTransaction())
+            {
+                try
+                {
+                    foreach (var insert in inserts)
+                        _repository.Session.Save(new CADAccount { cad = new User { id = cadId }, account = new Account { id = insert } });
+
+                    foreach (var delete in deletes)
+                        _repository.Session.Delete(delete);
+
+                    transaction.Commit();
+                }
+                catch (Exception ex)
+                {
+                    transaction.Rollback();
+                    throw ex;
+                }
+            }
         }
 
     }
--- MVC_Project.WebBackend/Controllers/CADController.cs
+++ MVC_Project.WebBackend/Controllers/CADController.cs
@@ -38,14 +38,59 @@ namespace MVC_Project.WebBackend.Controllers
         [HttpGet, AllowAnonymous]
         public ActionResult GetAccounts(Int64 id)
         {
-            var assigneds = _cadAccountService.GetAll().Select(x => x.account.id);
-            var availables = _accountService.FindBy(x => !assigneds.Contains(x.id)).Select(x => new { id = x.id, name = x.name + "( " + x.rfc + " )" });
-
-            return new JsonResult
+            try
             {
-                Data = new { success = true, accounts = availables },
-                JsonRequestBehavior = JsonRequestBehavior.AllowGet
-            };
+                var assigneds = _cadAccountService.GetAll().Select(x => x.account.id);
+                var availables = _accountService.FindBy(x => !assigneds.Contains(x.id)).
+                    Select(x => new { id = x.id, name = x.name + " ( " + x.rfc + " )" });
+                var assignedToCAD = _cadAccountService.FindBy(x => x.cad.id == id).
+                    Select(x => new { id = x.account.id, name = x.account.name + " ( " + x.account.rfc + " )" });
+
+                return new JsonResult
+                {
+                    Data = new { success = true, availables = availables, assigneds = assignedToCAD },
+                    JsonRequestBehavior = JsonRequestBehavior.AllowGet
+                };
+            }
+            catch(Exception ex)
+            {
+                return new JsonResult
+                {
+                    Data = new { success = false, message = ex.Message },
+                    JsonRequestBehavior = JsonRequestBehavior.AllowGet
+                };
+            }
+        }
+
+        [HttpPost, AllowAnonymous]
+        public ActionResult SetAccounts(Int64 cadId, List<Int64> accountIds)
+        {
+            try
+            {
+                if (accountIds == null)
+                    accountIds = new List<Int64>();
+
+                var assignedToCAD = _cadAccountService.FindBy(x => x.cad.id == cadId).
+                    Select(x => x);
+
+                var inserts = accountIds.Where(x => !assignedToCAD.Select(y => y.account.id).Contains(x));
+                var deletes = assignedToCAD.Where(x => !accountIds.Contains(x.account.id));
+                _cadAccountService.AssignCustomers(cadId, inserts, deletes);
+
+                return new JsonResult
+                {
+                    Data = new { success = true },
+                    JsonRequestBehavior = JsonRequestBehavior.AllowGet
+                };
+            }
+            catch(Exception ex)
+            {
+                return new JsonResult
+                {
+                    Data = new { success = false, message = ex.Message },
+                    JsonRequestBehavior = JsonRequestBehavior.AllowGet
+                };
+            }
         }
     }
 }
